c0d636b578172a32347e2b5098fd0e813214f692
Kshitij KArthick <kshitij.karthick@gmail.com>
2014-11-15 22:36:15 +0530

    Added Power,Comparison Operations with Individual Operator Functionality

--- src/rational.scala
+++ src/rational.scala
@@ -23,8 +23,17 @@ class RationalNumber(n: Int,d: Int){
 	def multiply(secondary: Int):RationalNumber=new RationalNumber(numerator*secondary,denominator)
 	def divide(secondary: RationalNumber):RationalNumber=new RationalNumber(numerator*secondary.denominator,denominator*secondary.numerator)
 	def divide(secondary: Int):RationalNumber=new RationalNumber(numerator,denominator*secondary)
-	def lessThan(secondary: RationalNumber):Boolean= {if(this.numerator*secondary.denominator<secondary.numerator*this.denominator)return true else false}
+	def lessThan(secondary: RationalNumber):Boolean={if(this.numerator*secondary.denominator<secondary.numerator*this.denominator)return true else false}
+	def isEqual(secondary: RationalNumber):Boolean={if(secondary.numerator==numerator && secondary.denominator==denominator)true else false}
+	def ==(secondary: RationalNumber):Boolean=(isEqual(secondary))
 	def max(secondary: RationalNumber)=(if(this.lessThan(secondary))secondary else this)
 	def min(secondary: RationalNumber)=(if(this.lessThan(secondary))this else secondary)
+	def pow(secondary: Int):RationalNumber={
+		if(secondary>0) 
+			new RationalNumber(math.pow(numerator,secondary).toInt,math.pow(denominator,secondary).toInt) 
+		else 
+			new RationalNumber(math.pow(denominator,math.abs(secondary)).toInt,math.pow(numerator,math.abs(secondary)).toInt)
+	}
+	def ^(secondary: Int):RationalNumber=(pow(secondary))
 	private def gcd(a: Int, b: Int): Int = (if(b==0) a else gcd(b,a%b))
 }
